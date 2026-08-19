45fedc30590dbd218c7e060405387312d47d8461
Jeroen Engelberts <u0369169071@gmail.com>
2026-08-19 19:56:45 +0000

    Verbeteringen in start_services

--- start_services.sh
+++ start_services.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Check whether in directory containing html
+if [ ! -d ${PWD}/html ]; then
+    echo "Please run from the directory where html is located!"
+    exit
+fi
+
 # Figure out who I am
 myself=$(whoami)
 
-# Initialize MariaDB
+# Start MariaDB
 tmpfile=$(mktemp)
 cat > ${tmpfile} << EOF
 #!/bin/bash
@@ -11,10 +17,10 @@ mariadbd-safe
 nohup mariadbd --user=mysql >& /dev/null &
 EOF
 chmod +x ${tmpfile}
-echo "Running MariaDB daemon in safe mode for 10 seconds..."
+echo "Running MariaDB daemon in safe mode for 3 seconds..."
 sudo nohup ${tmpfile} >& /dev/null &
 # Time to do something as regular user (with sudo)
-sleep 10
+sleep 3
 echo "Shutting down Mariab DB daemon in safe mode..."
 echo SHUTDOWN | sudo mariadb -u root
 rm ${tmpfile}
@@ -25,7 +31,7 @@ echo ""
 if [ -d /var/www/html ]; then
     echo "Setting up Apache HTML directory"
     sudo rm -rf /var/www/html
-    sudo ln -s /workspaces/${RepositoryName}/html /var/www/html
+    sudo ln -s ${PWD}/html /var/www/html
 fi
 sudo /usr/sbin/apachectl start
 echo "Apache should be running op port 8081"
